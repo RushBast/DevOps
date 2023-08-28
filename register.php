74411597f2c7e62bf377709ac380b1de562027f1
Pravij Gupta <102358152+RushBast@users.noreply.github.com>
2023-08-28 23:38:29 +0530

    commit 1.0

---
+++ register.php
@@ -0,0 +1,36 @@
+<?php
+// Establish database connection
+$servername = "localhost:3306";
+$username = "root";
+$password = "root";
+$dbname = "recipebook";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Process form submission
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $password = $_POST["psw"];
+    
+    // Validate and sanitize the input as per your requirements
+    
+    // Insert user data into the Users table
+    $sql = "INSERT INTO Users (username, email, password) VALUES ('$username', '$email', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "User registered successfully.";
+        header("refresh:2 url=login.php");
+                exit();
+    } else {
+        echo "Error registering user: " . $conn->error;
+    }
+}
+
+// Close the database connection
+$conn->close();
+?>
