79f4c5adf010aee1639abfae92cfb6fe81800b78
Megha <meghasuresha.cs21@bmsce.ac.in>
2023-09-03 16:45:51 +0530

    RECIPE BOOK

--- php/index.php
+++ php/index.php
@@ -139,7 +139,7 @@ $conn->close();
     </nav>
 
     <section class="home">
-        <div class="text"><b>Recipe Book</b></div>
+        <div class="text"><b>RECIPE BOOK</b></div>
         <br><div class="text">Welcome, <?php echo $username; ?>!</div>
         <div class="alang">
         <a href="indian.php"><div class="segment"> Indian</div></a>
